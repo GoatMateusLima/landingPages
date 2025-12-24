985dacc063ecabafab1bfaa9750160b9e66c472a
Mateus lima Bispo <mateuslimabispo15@gmail.com>
2025-12-24 02:59:21 -0300

    criei mais coisa, aprendi sobre variavel css e efeitos

--- bebibum/scripst.js
+++ bebibum/scripst.js
@@ -1,10 +1,20 @@
 let lista = document.querySelectorAll('.item');
 let next = document.getElementById('next');
 let prev = document.getElementById('prev');
+let main = document.querySelector('main');
 
 
 let contagem = lista.length;
 let active = 0
+
+
+function updateBackground() {
+        const activeItem = document.querySelector(".item.active");
+    if (!activeItem) return;
+
+    const bg = getComputedStyle(activeItem).getPropertyValue("--background");
+    document.documentElement.style.setProperty("--bg-atual", bg);
+    }
 
 next.onclick =  () =>{
     let activeOld = document.querySelector('.active');
@@ -12,6 +22,8 @@ next.onclick =  () =>{
 
     active = active >= contagem -1 ? 0 : active +1;
     lista[active].classList.add('active');
+
+    updateBackground();
 }
 
 prev.onclick = () =>{
@@ -20,5 +32,13 @@ prev.onclick = () =>{
 
     active = active <= 0 ? contagem -1 : active -1;
     lista[active].classList.add('active');
+
+    updateBackground();
+
+    
 }
+
+    
+    // Update background on page load
+    updateBackground();
 
